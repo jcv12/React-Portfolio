93bbfa7c288d4823cdd3a586033c3714e1da863e
jcv12 <john.c.vayianos@gmail.com>
2022-06-21 20:47:30 -0400

    setting up the nav bar

---
+++ src/components/Nav/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Nav() {
+
+
+    return(
+    <header>
+        <h1> JCV </h1>
+        <nav>
+            <ul>
+                <li> <a href="#what-we-do">asdasd</a> </li>
+                <li> <a href="#what-you-do">ddds</a> </li>
+                <li> <a href="#your-trainers">Yours</a> </li>
+                <li> <a href="#reach-out">Rt</a> </li>
+            </ul>
+        </nav>
+    </header>
+    );
+}
+
+
+
+export default Nav;
