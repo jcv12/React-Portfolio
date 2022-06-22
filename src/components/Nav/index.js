69188079c93d49c3279989bb56b7d1b70e05c456
jcv12 <john.c.vayianos@gmail.com>
2022-06-21 23:00:56 -0400

    fixed the resume link

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -11,7 +11,7 @@ function Nav() {
                 <li> <a href="/">About</a> </li>
                 <li> <a href="/">Projects</a> </li>
                 <li> <a href="/">Contact</a> </li>
-                <li> <a href="/">Resume</a> </li>
+                <li> <a href="https://docs.google.com/document/d/1g-nBKzXDXOVsQBTBirO5DfCwKb5gMlMPxGIzPCM0QUQ/edit?usp=sharing">Resume</a> </li>
             </ul>
         </nav>
     </header>
