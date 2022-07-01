1b9a7ffa10e5cbd1fe05eefc98efa24c2e96c5ce
jcv12 <john.c.vayianos@gmail.com>
2022-06-30 20:31:10 -0400

    added those stupid hover lines

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,19 +3,19 @@ import React from "react";
 function Contact() {
 
     return(
-    <header>
+    <header class="footer">
         
         <h1>Contact Me</h1>
         <nav>
             <ul>
             <li>
-                <a href="https://github.com/jcv12">Github</a>
+                <a href="https://github.com/jcv12" class="sliding-middle-out">Github</a>
             </li>
             <li>
-                <a href="mailto: john.c.vayianos@gmail.com">Gmail</a>
+                <a href="mailto: john.c.vayianos@gmail.com" class="sliding-middle-out">Gmail</a>
             </li>
             <li>
-                <a href="https://www.linkedin.com/in/john-vayianos-114257228/">Linkedin</a>
+                <a href="https://www.linkedin.com/in/john-vayianos-114257228/" class="sliding-middle-out">Linkedin</a>
             </li>
             </ul>
         </nav>
